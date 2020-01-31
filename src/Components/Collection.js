dac75798097ed523710462d5854a862267838258
Brandon Warner <warner.brandon.a@gmail.com>
2020-01-31 11:16:50 -0600

    change collection component from class to function

--- src/Components/Collection.js
+++ src/Components/Collection.js
@@ -1,30 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Media from './Media';
 
 
-class Collection extends Component{
-    render(){
-        let mapMe = this.props.collection.map( (media, index) => {
-                return(
-                    <Media
-                        key={index}
-                        collection={media}
-                        updateCollection={this.props.updateCollections}
-                    />
-                );
-            }
-        );
-        return(
-            <main>
-                <h1 className="text-center mrg-btm-65">My Media Collection</h1>
-                <div className="collection-wrap flex justify-center flex-wrap">
-                    {mapMe}
-                </div>
-            </main>
-        );
-    }
-
+function Collection(props){
+    let mappedMedia = props.collection.map( (media, index) => {
+            return(
+                <Media
+                    key={index}
+                    collection={media}
+                    updateCollection={props.updateCollections}
+                />
+            );
+        }
+    );
+    return(
+        <main>
+            <h1 className="text-center mrg-btm-65">My Media Collection</h1>
+            <div className="collection-wrap flex justify-center flex-wrap">
+                {mappedMedia}
+            </div>
+        </main>
+    );
 }
+
+
 
 
 export default Collection;
